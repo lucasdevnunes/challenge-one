aef4268b815884d3671b8a371c2951aa16607e80
Lucas M Nunes <105028691+lucasdevnunes@users.noreply.github.com>
2024-07-23 17:18:26 -0300

    Mudanca da logica da criptografia, e criacao da descriptografia.

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
 function criptografar() {
-    //let mensagem = document.getElementById('mensagem').value;
-    let mensagem = 'ola tudo bem'
-    let mensagemCriptografada = '';
-
-    for (let letra of mensagem) {
-        if (letra === 'a'){
-            letra = 'ai';
-            mensagemCriptografada += letra;
-        } else if (letra === 'e'){
-            letra = 'enter';
-            mensagemCriptografada += letra;
-        } else if (letra === 'i'){
-            letra = 'imes';
-            mensagemCriptografada += letra;
-        } else if (letra === 'o'){
-            letra = 'ober';
-            mensagemCriptografada += letra;
-        } else if (letra === 'u'){
-            letra = 'ufat';
-            mensagemCriptografada += letra;
-        } else {
-            mensagemCriptografada += letra;
-        }
-    }
-
-    return mensagemCriptografada;
+    let mensagem = document.getElementById('mensagem').value;
+    let mensagemCriptografada = mensagem
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
+    document.getElementById('mensagemFinal').value = mensagemCriptografada;
 }
 
-function descriptografar() {}
+function descriptografar() {
+    let mensagem = document.getElementById('mensagem').value;
+    let mensagemDescriptografada = mensagem
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
+    document.getElementById('mensagemFinal').value = mensagemDescriptografada;
+}
 
 function copiarMensagem() {}
-
-console.log(criptografar());

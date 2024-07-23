b9f0fefe0ba061c68324bce56d5a2f04d3c19a8c
Lucas M Nunes <105028691+lucasdevnunes@users.noreply.github.com>
2024-07-23 12:45:02 -0300

    Criacao da funcao de criptografar.

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-function criptografar() {}
+function criptografar() {
+    //let mensagem = document.getElementById('mensagem').value;
+    let mensagem = 'ola tudo bem'
+    let mensagemCriptografada = '';
+
+    for (let letra of mensagem) {
+        if (letra === 'a'){
+            letra = 'ai';
+            mensagemCriptografada += letra;
+        } else if (letra === 'e'){
+            letra = 'enter';
+            mensagemCriptografada += letra;
+        } else if (letra === 'i'){
+            letra = 'imes';
+            mensagemCriptografada += letra;
+        } else if (letra === 'o'){
+            letra = 'ober';
+            mensagemCriptografada += letra;
+        } else if (letra === 'u'){
+            letra = 'ufat';
+            mensagemCriptografada += letra;
+        } else {
+            mensagemCriptografada += letra;
+        }
+    }
+
+    return mensagemCriptografada;
+}
 
 function descriptografar() {}
 
 function copiarMensagem() {}
+
+console.log(criptografar());
